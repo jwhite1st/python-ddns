1a99fde444898763ed65941f87d82f185675a742
Jacob <jake@jwhite.network>
2019-08-31 19:51:23 -0400

    Added record checking and updating

--- python-ddns.py
+++ python-ddns.py
@@ -4,9 +4,9 @@ import socket
 import requests
 # import ipaddress
 
-base_url = "https://api.cloudflare.com/client/v4/zones/"
-config = configparser.ConfigParser()
-config.read("config.conf")
+BASE_URL = "https://api.cloudflare.com/client/v4/zones/"
+CONFIG = configparser.ConfigParser()
+CONFIG.read("config.conf")
 
 # Shamlessly taken from stackoverflow https://stackoverflow.com/a/28950776/11542276
 def get_ip():
@@ -19,15 +19,21 @@ def get_ip():
         s.close()
     return host
 
+def check_record():
+    record = {}
+    record["type"] = "A"
+    record["name"] = CONFIG['Cloudflare']['name']
+    output = send(record, 1)
+    if output:
+        return output
 
 def add_record(ip):
     record = {}
     record["type"] = "A"
-    record["name"] = config['Cloudflare']['name']
+    record["name"] = CONFIG['Cloudflare']['name']
     record['content'] = ip
-    record['proxied'] = config['Cloudflare']['proxied'] == 'True'
-    output = post(record)
-    print(output)
+    record['proxied'] = CONFIG['Cloudflare']['proxied'] == 'True'
+    output = send(record, 2)
     if not output['success']:
         try:
             error_code = output['errors'][0]['error_chain'][0]['code']
@@ -36,7 +42,7 @@ def add_record(ip):
         # This error code means the record can not be proxied. Likely due to a private IP
         if error_code == 9041:
             record['proxied'] = False
-            r = post(record)
+            r = send(record, 2)
             if r.json()['success']:
                 print("The record was created successfully")
         else:
@@ -45,13 +51,36 @@ def add_record(ip):
     if output['success']:
         print("The record was created successfully")
 
+def update_record(ip, record_id):
+    record = {}
+    record["type"] = "A"
+    record["name"] = CONFIG['Cloudflare']['name']
+    record['content'] = ip
+    output = send(record, 3, record_id)
+    if not output['success']:
+        print("There was an error:")
+        print(output)
 
-def post(content):
-    headers = {'Authorization': config['Cloudflare']['API_Token'],
-               "X-Auth-Email": config['Cloudflare']['Email'],
+
+def send(content, which, extra=None):
+    headers = {'Authorization': CONFIG['Cloudflare']['API_Token'],
+               "X-Auth-Email": CONFIG['Cloudflare']['Email'],
                "Content-Type": "application/json"}
-    zone = config['Cloudflare']['Zone']
-    api_url = base_url+zone+"/dns_records"
-    return requests.post(api_url, json=content, headers=headers).json()
+    zone = CONFIG['Cloudflare']['Zone']
+    api_url = BASE_URL+zone+"/dns_records"
+    if which == 1:
+        r = requests.get(api_url, params=content, headers=headers).json()
+        if r['result']:
+            return r['result'][0]['id']
+    elif which == 2:
+        return requests.post(api_url, json=content, headers=headers).json()
+    elif which == 3:
+        api_url = api_url+"/"+extra
+        return requests.put(api_url, json=content, headers=headers).json()
 
-add_record(get_ip())
+
+check = check_record()
+if check:
+    update_record(get_ip(), check)
+else:
+    add_record(get_ip())
