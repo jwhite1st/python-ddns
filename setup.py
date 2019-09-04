01f1df7363057e043f4b7eaedd03c73ba0ff1c4f
Jacob <jake@jwhite.network>
2019-09-03 20:09:40 -0400

    Updated setup

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 # pylint: disable=invalid-name, missing-docstring, line-too-long
+import os
 from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name="Python DDNS",
-    version="0.0.1",
+    version="0.0.1 Build: $CI_BUILD_ID",
     author="Jacob White",
     author_email="jake@jwhite.network",
     install_requires=['requests'],
@@ -13,7 +17,16 @@ setup(
     project_urls={
         "Issues": "https://gitlab.com/jwhite1st/python-ddns/issues",
     },
-    license='MIT',
-    packages=['python-ddns']
-
+    license='GPL-3.0',
+    packages=['python-ddns'],
+    python_requires='>=3.6',
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: System :: Installation/Setup",
+        "Development Status :: 2 - Pre-Alpha"
+    ],
+    long_description=read('README.md'),
 )
