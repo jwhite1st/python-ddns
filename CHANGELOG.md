3b37f93f604ed2cd08e47ad3a32191157036e366
Jacob W <jake@jwhite.network>
2019-12-18 02:21:12 +0000

    Changes in CI Flow  - Testing Stages run in all builds  - The production stage only runs if there is a tag

---
+++ CHANGELOG.md
@@ -0,0 +1,39 @@
+<!-- markdownlint-disable MD024 -->
+# Changelog
+
+## [v2.0.0] - 2019-12-17
+
+### Added
+
+- Providers folder. Will allow for easier creation of other providers
+
+### Changed
+
+- Changed Cloudflare from a class to a function.
+- Renamed python_ddns to pddns.
+- Changed how versioning worked.
+
+### Deleted
+
+- Cleaned up exposed secrets
+- Removed .flake8 file
+- Removed .travis.yml
+- Remove the test for python 3.4 because it is no longer supported
+
+## [v1.1] - 2019-10-17
+
+## Changed
+
+- Cloudflare as a separate script
+
+## [v1.0] - 2019-10-16
+
+### Added
+
+- First version with pypi package
+  
+## [0.0.2] 2019-09-08
+
+### Added
+
+- First working version
--- setup.py
+++ setup.py
@@ -1,33 +1,30 @@
-# pylint: disable=invalid-name, missing-docstring, line-too-long
+"""Sets up Python-DDNS to be installed"""
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
+from pddns import __version__
 
-# https://medium.com/@pypripackages/using-gitlab-pipelines-to-deploy-python-packages-in-production-and-staging-environments-8ab7dc979274
-if os.environ.get('CI_COMMIT_TAG'):
-    v = os.environ['CI_COMMIT_TAG']
-else:
-    try:
-        v = os.environ['CI_COMMIT_SHORT_SHA']
-    except KeyError as ke:
-        v = "local_build"
 
 def read(fname):
+    """Reads README.md as long description"""
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(
     name="Python DDNS",
-    version=v,
+    version=__version__,
     author="Jacob White",
     author_email="jake@jwhite.network",
     install_requires=['requests'],
-    package_data={'': ['config.conf']},
-    description="A DDNS client written in python that updates the A record on Cloudflare with the current IP of this device.",
-    url="https://gitlab.com/jwhite1st/python-ddns",
+    description="A DDNS client that updates with current IP."
+                "Currently only Cloudflare is supported.",
+    url="https://gitlab.com/Cyb3r-Jak3/python-ddns",
     project_urls={
-        "Issues": "https://gitlab.com/jwhite1st/python-ddns/issues",
+        "Issues": "https://gitlab.com/Cyb3r-Jak3/python-ddns/issues",
     },
+    data_files=["pddns/config.dist.conf"],
     license='GPL-3.0',
-    packages=['python_ddns'],
+    packages=find_packages(),
+    include_package_data=True,
     python_requires='>=3',
     classifiers=[
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
@@ -35,11 +32,11 @@ setup(
         "Natural Language :: English",
         "Programming Language :: Python :: 3 :: Only",
         "Topic :: System :: Installation/Setup",
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 2 - Pre-Alpha",
     ],
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
     entry_points={
-        "console_scripts": ['python-ddns=python_ddns.python_ddns:main',]
-        },
+        "console_scripts": ['pddns=pddns.pddns:main']
+    }
 )
